4ea09b7ef4fb6b9bd8ce9a61b3c957b0810d77ce
FiammaV <ines.perdigao.nunes@gmail.com>
2018-12-26 12:45:03 +0000

    Started World.cs

---
+++ World.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame {
+    class World {
+
+        public Random rnd = new Random();
+        public ConsoleKeyInfo pressedKey = new ConsoleKeyInfo();
+
+        public int noseX;
+        public int noseY;
+        public int nTail;
+        public int appleX;
+        public int appleY;
+        public int points;
+
+        public int[] TailX = new int[100];
+        public int[] TailY = new int[100];
+
+        public const int height = 20;
+        public const int width = 60;
+
+        public bool gameOver;
+        public bool reset;
+        public bool isPrinted;
+        public bool horizontal;
+        public bool vertical;
+        public string dir;
+        public string pre_dir;
+    }
+}
